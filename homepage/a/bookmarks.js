4aeddfcbd4c0c380420c08ab0a7b42fbc5327a47
Spacey_Core <59103833+Exaby@users.noreply.github.com>
2022-12-06 15:43:22 -0500

    add primary

---
+++ homepage/a/bookmarks.js
@@ -0,0 +1,32 @@
+const bookmarks = [
+  {
+    title: "Daily",
+    links: [
+      { name: "Inbox", url: "https://inbox.google.com" },
+      { name: "Drive", url: "https://cloud.spco.xyz/" },
+    ],
+  },
+  {
+    title: "Media",
+    links: [
+      { name: "Youtube", url: "https://youtube.com" },
+      { name: "Netflix", url: "https://netflix.com" },
+      { name: "Amazon Prime", url: "https://www.primevideo.com/" },
+      { name: "Disney Plus", url: "https://www.disneyplus.com/" },
+    ],
+  },
+  {
+    title: "Social",
+    links: [
+      { name: "Twitter", url: "https://twitter.com" },
+      { name: "Facebook", url: "https://facebook.com" },
+    ],
+  },
+  {
+    title: "Misc",
+    links: [
+      { name: "SR Wiki", url: "https://sunrust.org" },
+      { name: "GitHub", url: "https://github.com" },
+    ],
+  },
+];
---
+++ homepage/bookmarks.js
@@ -0,0 +1,30 @@
+// Note: having length != 4 will mess with layout based on how the site is styled
+const bookmarks = [
+  {
+    title: "Daily",
+    links: [
+      { name: "Inbox", url: "https://inbox.google.com" },
+    ],
+  },
+  {
+    title: "Media",
+    links: [
+      { name: "Youtube", url: "https://youtube.com" },
+      { name: "Netflix", url: "https://netflix.com" },
+      { name: "Amazon Prime", url: "https://www.primevideo.com/" },
+    ],
+  },
+  {
+    title: "Social",
+    links: [
+      { name: "Twitter", url: "https://twitter.com" },
+      { name: "Facebook", url: "https://facebook.com" },
+    ],
+  },
+  {
+    title: "Misc",
+    links: [
+      { name: "GitHub", url: "https://github.com" },
+    ],
+  },
+];
