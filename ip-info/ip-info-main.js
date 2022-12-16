e0ac4cb0697d1fdd5851944cedade25dc836e0b5
Keaton Sanders <keatonpirate@gmail.com>
2022-12-16 15:29:14 -0500

    cleanup pt. 2/unkown

--- ip-info/ip-info-main.js
+++ ip-info/ip-info-main.js
@@ -1,42 +1,70 @@
-function main() {
-    var ip = "";
-    var aso = "";        
-    var asn = "";    
-    var continent = "";
-    var cc = "";
-    var country = "";
-    var city = "";
-    var postal = "";
-    var latitude = "";
-    var longitude = "";
-    var tz = "";
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            ip = JSON.parse(this.responseText).ip;
-            aso = JSON.parse(this.responseText).aso;
-            asn = JSON.parse(this.responseText).asn;
-            continent = JSON.parse(this.responseText).continent;
-            cc = JSON.parse(this.responseText).cc;
-            country = JSON.parse(this.responseText).country;
-            city = JSON.parse(this.responseText).city;
-            postal = JSON.parse(this.responseText).postal;
-            latitude = JSON.parse(this.responseText).latitude;
-            longitude = JSON.parse(this.responseText).longitude;
-            tz = JSON.parse(this.responseText).tz;
-            document.getElementById("ip-info-ip").innerHTML = ip;
-            document.getElementById("ip-info-aso").innerHTML = aso;
-            document.getElementById("ip-info-asn").innerHTML = asn;
-            document.getElementById("ip-info-continent").innerHTML = continent;
-            document.getElementById("ip-info-cc").innerHTML = cc;
-            document.getElementById("ip-info-country").innerHTML = country;
-            document.getElementById("ip-info-city").innerHTML = city;
-            document.getElementById("ip-info-postal").innerHTML = postal;
-            document.getElementById("ip-info-latlng").innerHTML = latitude + ", " + longitude;
-            document.getElementById("ip-info-tz").innerHTML = tz;
-        }
-    };
-    xmlhttp.open("GET", "https://ident.me/json", true);
-    xmlhttp.send();  
-}
-main(); // call the function right away
+let e = "",
+    n = "",
+    t = "",
+    s = "",
+    i = "",
+    p = "",
+    r = "",
+    o = "",
+    a = "",
+    T = "",
+    d = "",
+    l = new XMLHttpRequest();
+(l.onreadystatechange = function () {
+            e = JSON.parse(this.responseText).ip
+            n = JSON.parse(this.responseText).aso
+            t = JSON.parse(this.responseText).asn
+            s = JSON.parse(this.responseText).continent
+            i = JSON.parse(this.responseText).cc
+            p = JSON.parse(this.responseText).country
+            r = JSON.parse(this.responseText).city
+            o = JSON.parse(this.responseText).postal
+            a = JSON.parse(this.responseText).latitude
+            T = JSON.parse(this.responseText).longitude
+            d = JSON.parse(this.responseText).tz
+            if (e == "") {
+                e = "Not Found"
+            }
+            if (n == "") {
+                n = "Not Found"
+            }
+            if (t == "") {
+                t = "Not Found"
+            }
+            if (s == "") {
+                s = "Not Found"
+            }
+            if (i == "") {
+                i = "Not Found"
+            }
+            if (p == "") {
+                p = "Not Found"
+            }
+            if (r == "") {
+                r = "Not Found"
+            }
+            if (o == "") {
+                o = "Not Found"
+            }
+            if (a == "") {
+                a = "Not Found"
+            }
+            if (T == "") {
+                T = "Not Found"
+            }
+            if (d == "") {
+                d = "Not Found"
+            }
+            document.getElementById("ip-info-ip").innerHTML = "IP: " + e
+            document.getElementById("ip-info-aso").innerHTML = "ASO: " + n
+            document.getElementById("ip-info-asn").innerHTML = "ASN: " + t
+            document.getElementById("ip-info-continent").innerHTML = "Continent: " + s
+            document.getElementById("ip-info-cc").innerHTML = "CC: " + i
+            document.getElementById("ip-info-country").innerHTML = "Country: " + p
+            document.getElementById("ip-info-city").innerHTML = "City: " + r
+            document.getElementById("ip-info-postal").innerHTML = "Postal: " + o
+            document.getElementById("ip-info-latlng").innerHTML = "Latlng: " + a + ", " + T
+            document.getElementById("ip-info-tz").innerHTML = "Timezone: " + d
+}),
+    l.open("GET", "https://ident.me/json", !0),
+    l.send();
